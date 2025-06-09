f6f664a5d1730d16ac0198ab4683d5dd2134b74f
Shifani-golda <shifanigolda@karunya.edu.in>
2025-06-09 16:35:40 +0530

    Final cleaned commit for SerenSoak project

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dummy-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Serensoak.onrender.com']
+
 
 
 # Application definition
@@ -120,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
